15b58224c7ce3181e99e0ce5934f9634c3b3665a
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2026-04-19 16:49:18 +0300

    feat:Make lab2.

--- Nimble.Modulith.AppHost/AppHost.cs
+++ Nimble.Modulith.AppHost/AppHost.cs
@@ -1,3 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var sqlServer = builder.AddSqlServer("sqlserver")
+    .WithDataVolume();
+
+var usersDb = sqlServer.AddDatabase("usersdb");
+
+builder.AddProject<Projects.Nimble_Modulith_Web>("webapi")
+    .WithReference(usersDb)
+    .WaitFor(usersDb);
+
 builder.Build().Run();
--- Nimble.Modulith.Web/Program.cs
+++ Nimble.Modulith.Web/Program.cs
@@ -1,41 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using FastEndpoints;
+using FastEndpoints.Security;
+using FastEndpoints.Swagger;
+using Nimble.Modulith.Users;
+using Serilog;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var logger = Log.Logger = new LoggerConfiguration()
+  .Enrich.FromLogContext()
+  .WriteTo.Console()
+  .CreateLogger();
+
+logger.Information("Starting web host");
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
+
+builder.AddServiceDefaults();
+
+builder.Services.AddFastEndpoints()
+    .AddAuthenticationJwtBearer(s =>
+    {
+        s.SigningKey = builder.Configuration["Auth:JwtSecret"];
+    })
+    .AddAuthorization()
+    .SwaggerDocument();
+
+builder.AddUsersModuleServices(logger);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.UseHttpsRedirection();
+app.UseFastEndpoints()
+    .UseSwaggerGen();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+await app.EnsureUsersModuleDatabaseAsync();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
